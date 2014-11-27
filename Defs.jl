1cdcfc0cc5cb2edf4ce214c3d6b1a8e0db44b1a6
Martin Zumaya <sstereo.vision@gmail.com>
2014-11-27 12:40:10 -0600

    Se agrega archivo nativo de julia

--- Defs.jl
+++ Defs.jl
@@ -1,7 +1,7 @@
 
 
 #Genera N vectores aleatorios de dimension dim
-function SetPos()
+function StartPos()
     for i = 1:N
         #push!(pos,rand(-L:L,dim)) #inicia en una caja de tamanio L
         push!(pos,rand(dim)*L) #inicia en una caja de tamanio L
@@ -10,22 +10,24 @@ function SetPos()
 end
 
 #Normaliza los vectores, magnitud v0 direccion aleatoria
-function SetVels()
+function StartVels(v0)
     for i in 1:N
         vel[i] = vel[i] * (v0/norm(vel[i]))
         #println(norm(vel[i]))
     end
 end
 
-#calcula distancias y adjacencia
-function SetMatrix()
-    Adj = zeros(N,N) #Limpia matriz adjacencia
-    Dist = zeros(N,N) #Limpia matriz adjacencia
+#calcula distancias y adjacencia local
+function SetMatrix(r0) 
+
+    Adj = zeros(N,N) #Limpia matriz 
+    Dist = zeros(N,N) #Limpia matriz 
 
     for i = 1:N
         for j = N:-1:i
             
             d = norm(pos[i]-pos[j])
+
             Dist[i;j] = Dist[j;i] = d
             
             #d < r0 && d > 0 ? Adj[i;j] = Adj[j;i] = 1 : Adj[i;j] = 0
@@ -36,34 +38,85 @@ end
 
 #Actualiza posiciones
 function UpdatePos()
-    #println(pos)
+
     for i = 1:N
         pos[i] = pos[i] + vel[i] 
     end
-#println(pos)
+
 end
 
-#Actualiza velocidades
+#Calcula las direcciones de la velocidad promedio de cada vecindad
+function GetAngs(A,vel)
+
+    angs = Float64[] #Arreglo para guardar angulos
+
+    # println(angs)
+    # Calcula los angulos de las velocidades promedio
+
+    for i = 1:size(A)[1]
+
+        U = vel'.*A[i;:] #Determina la vecindad
+        k = sum(A[i;:]) #Numero de parts en la vecindad 
+
+        v_prom = 1/k * sum(U) #Calcula vector promedio
+    #    println(degs(atan2(v[2],v[1])))
+
+        # a = atan2(v_prom[2],v_prom[1]) #Direccion del vector promedio
+        # push!(angs,a) #agrega el angulo al arreglo
+        # # println("part $i -> ang prom $a")
+
+        push!(angs,atan2(v_prom[2],v_prom[1])) #agrega el angulo al arreglo
+
+    end
+end
+
+#Construye red aleatoria de conectividad k
+
+# ==================================== Parametros ==============================================
+
+    # dt -> delta t
+    # f -> fraccion de conexiones aleatorias relativo al total de parts
+    # Vo -> magnitud de la velocidad de las particulas
+    # ht ->ruido topologico
+    # hg -> ruido geometrico
+    # pg -> peso de vecindad geometrica
+    # p  -> densidad
+    # psi -> parametro de orden
+    # l  -> regimen de velocidad (tamanio de paso relativo al radio de interaccion)
+    # r -> radio de interaccion
+    # N -> numero de particulas
+
+#Valores default de parametros
+
+dim= 2 
+
+dt = 1
+f  = 0.1
+v0 = 1
+ht = 0.3
+hg = 0.3
+p = 1
+l = 0.4
+L = 30 # Tamaño caja inicial
 
 N = 4
-dim= 2
-v0 = 1
+
 pos = Array[] #Vector de posiciones
 vel = Array[] #Vector de velocidades
+
 Dist = zeros(N,N) #Matriz de distancias
-Adj = zeros(N,N) #Matriz de adjacencia
+
+SR_I = zeros(N,N) #Interacciones de corto alcanze
+LR_I = zeros(N,N) #Interacciones de lanrgo alcanze -> NO CAMBIA en tiempo
 
 r0 = 3.
 L = 2.
 
 
-SetPos()
-SetVels()
-SetMatrix()
+StartPos()
+StartVels(v0)
+SetMatrix(r0)
 println("posiciones:")
 println(pos)
 println("velocidades:")
 println(vel)
-
-
-
