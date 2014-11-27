7c78ce4743db674253c205e499996536d55c92da
Martin Zumaya <sstereo.vision@gmail.com>
2014-11-26 19:48:04 -0600

    Se agregan archivos nativos de julia

---
+++ Defs.jl
@@ -0,0 +1,69 @@
+
+
+#Genera N vectores aleatorios de dimension dim
+function SetPos()
+    for i = 1:N
+        #push!(pos,rand(-L:L,dim)) #inicia en una caja de tamanio L
+        push!(pos,rand(dim)*L) #inicia en una caja de tamanio L
+        push!(vel,rand(dim))
+    end
+end
+
+#Normaliza los vectores, magnitud v0 direccion aleatoria
+function SetVels()
+    for i in 1:N
+        vel[i] = vel[i] * (v0/norm(vel[i]))
+        #println(norm(vel[i]))
+    end
+end
+
+#calcula distancias y adjacencia
+function SetMatrix()
+    Adj = zeros(N,N) #Limpia matriz adjacencia
+    Dist = zeros(N,N) #Limpia matriz adjacencia
+
+    for i = 1:N
+        for j = N:-1:i
+            
+            d = norm(pos[i]-pos[j])
+            Dist[i;j] = Dist[j;i] = d
+            
+            #d < r0 && d > 0 ? Adj[i;j] = Adj[j;i] = 1 : Adj[i;j] = 0
+            d < r0 ? Adj[i;j] = Adj[j;i] = 1 : Adj[i;j] = 0
+        end
+    end
+end
+
+#Actualiza posiciones
+function UpdatePos()
+    #println(pos)
+    for i = 1:N
+        pos[i] = pos[i] + vel[i] 
+    end
+#println(pos)
+end
+
+#Actualiza velocidades
+
+N = 4
+dim= 2
+v0 = 1
+pos = Array[] #Vector de posiciones
+vel = Array[] #Vector de velocidades
+Dist = zeros(N,N) #Matriz de distancias
+Adj = zeros(N,N) #Matriz de adjacencia
+
+r0 = 3.
+L = 2.
+
+
+SetPos()
+SetVels()
+SetMatrix()
+println("posiciones:")
+println(pos)
+println("velocidades:")
+println(vel)
+
+
+
