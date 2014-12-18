ba21e1c1c5318c3997d2511760448ab74b96b680
Martin Zumaya <sstereo.vision@gmail.com>
2014-12-18 13:21:12 -0600

    parametros

--- PyData/bash_script.py
+++ PyData/bash_script.py
@@ -5,13 +5,13 @@ nombre = '../Julia/runs_script.sh'
 
 script = open(nombre,'w')
 
-N = 12
+N = 20
 s = 0.05
 
 script.write("#!/bin/bash\n")
 
 for i in range(1,N+1):
-  if i%8 == 0:
+  if i%6 == 0:
     script.write( "julia simul.jl " + repr(round(i*s,2)) + "\n")
   else:
     script.write( "nohup julia simul.jl " + repr(round(i*s,2)) + " &\n")
