8e597286f7c2e9274e8ecc704d87fd0dd08618d3
Martin Zumaya <sstereo.vision@gmail.com>
2014-12-18 12:53:01 -0600

    Arreglo de cosas
    Mas “julia like” un poco de optimización

--- Julia/flock_lib.jl
+++ Julia/flock_lib.jl
@@ -1,58 +1,140 @@
-module libflock
+########################
 
-# using #dependencies
+# Martin Zumaya Hernandez
+# Diciembre 2014
+# Libreria para flocks
 
-# import #methods to overload
-
-# export SetPos, SetVels, SetMatrix, UpdatePos
-export SetPos, SetVels
-
- #module body
+########################
 
 #Genera N vectores aleatorios de dimension dim
-function SetPos()
-    for i=1:N
-        #push!(pos,rand(-L:L,dim)) #inicia en una caja de tamanio L
-        push!(pos,rand(dim)*L) #inicia en una caja de tamanio L
-        push!(vel,rand(dim))
+
+function StartVecs(L::Float64, vel::Array{Array{Float64,1},1}, pos::Array{Array{Float64,1},1})
+    for i = 1:N
+        push!(pos,[rand(-L:L),rand(-L:L)]) #inicia en una caja de tamanio L
+        push!(vel,[rand(),rand()])
     end
 end
+
+########################################################
 
 #Normaliza los vectores, magnitud v0 direccion aleatoria
-function SetVels()
+
+function StartVels(v0::Float64, vel::Array{Array{Float64,1},1})
     for i in 1:N
-        vel[i]= vel[i] * (v0/norm(vel[i]))
-        #println(norm(vel[i]))
+        scale!(vel[i],v0/norm(vel[i])) #escala y modifica vel[i]
     end
 end
 
-#calcula distancias y adjacencia
-function SetMatrix()
-    # Adj = zeros(N,N) #Limpia matriz adjacencia
-    # Dist = zeros(N,N) #Limpia matriz adjacencia
-    # Usando sparse
-    Adj=spzeros(N,N) #Limpia matriz adjacencia
-    Dist=spzeros(N,N) #Limpia matriz adjacencia
+########################################################
 
-    for i=1:N
-        for j=N:-1:i
-            
-            d=norm(pos[i]-pos[j])
-            Dist[i;j]=Dist[j;i]=d
-            
-            #d < r0 && d > 0 ? Adj[i;j] = Adj[j;i] = 1 : Adj[i;j] = 0
-            d < r0 ? Adj[i;j]=Adj[j;i]=1 : Adj[i;j]=0
+#Actualiza posiciones
+
+function UpdatePos(pos::Array{Array{Float64,1},1},dt::Float64)
+    broadcast!(+,pos,pos,scale(vel,dt))
+end
+
+########################################################
+
+#Construye red aleatoria de conectividad k
+
+function SetLR(k::Int64,X::SparseMatrixCSC{Float64,Int64}) #con sparse
+    #Matriz aleatoria
+    for i = 1:size(X)[1]
+        for j = 1:k        
+            switch = true
+            while switch 
+                s = rand(1:N)
+                if s != i
+                    X[i,s] = 1
+                    #X[i,s] = X[s,i] = 1 
+                    switch = false
+                end
+            end
         end
     end
 end
 
-#Actualiza posiciones
-function UpdatePos()
-    #println(pos)
-    for i = 1:N
-        pos[i]= pos[i] + vel[i] 
+########################################################
+
+#calcula distancias y adjacencia local
+function SetSR(r0::Float64,Dist::Array{Float64,2})
+
+    I = Int64[]
+    J = Int64[]
+
+    for i = 1:N , j = N:-1:i
+            
+        d = norm(pos[i]-pos[j])
+
+        Dist[i;j] = Dist[j;i] = d
+        # Dist[j;i] = d
+        
+        if d < r0  
+            push!(I,i)
+            push!(J,j)
+        end
     end
-#println(pos)
+
+    # Dist += Dist'
+
+    return sparse(vcat(I,J),vcat(J,I),ones(2*size(I)[1]))
 end
 
+########################################################
+
+#Calcula las direcciones de la velocidad promedio de cada vecindad
+function GetAngs(vel::Array{Array{Float64,1},1}, A::SparseMatrixCSC{Float64,Int64}) #con sparse
+
+    angs = Array(Float64,size(A)[1])
+
+    for i = 1:size(A)[1]
+
+        U = sum(broadcast(*,vel,A[:;i])) #com sparse
+
+        k = nnz(A[:;i]) #Numero de parts en la vecindad 
+
+        scale!(U,1/k)
+
+        angs[i] = atan2(U[2],U[1]) #agrega el angulo al arreglo
+    end
+
+    return angs
 end
+
+########################################################
+
+#Rota vector 2D
+
+function RotVec(vec::Array{Float64,1},alpha::Float64)
+
+    M = [cos(alpha) sin(alpha) ; -sin(alpha) cos(alpha)]
+
+    return M*vec
+end
+
+########################################################
+
+#Actualiza velocidades
+
+function UpdateVel(vel::Array{Array{Float64,1},1},SR::SparseMatrixCSC{Float64,Int64},LR::SparseMatrixCSC{Float64,Int64})
+
+    AS = GetAngs(vel,SR) #Angulos inter corto
+    AL = GetAngs(vel,LR) #Angulos inter largo
+
+    for i = 1:N
+
+        eta = rand(-pi:pi,2) # Vector de angs aleatorios
+
+        ang = ruido.*eta # aleaorios * eta
+
+        ang_tot = abs(1-w) * (AL[i]+ang[1]) + w * (AS[i]+ang[2])
+
+        # vel_n = RotVec(vel[i],ang_tot)
+        vel[i] = RotVec(vel[i],ang_tot)
+        
+        # vel[i] = vel_n
+
+    end
+end
+
+########################################################
--- Julia/simul.jl
+++ Julia/simul.jl
@@ -10,7 +10,7 @@
 include("flock_lib.jl")
 
 #Escribe trayectorias
-function PrintTrays(pos::Array{Array{Float64,2},1})
+function PrintTrays(pos::Array{Array{Float64,1},1})
     for i = 1:N
         @inbounds rr = repr(pos[i])
         write(trays,rr[2:end-1])
@@ -37,11 +37,12 @@ function PrintParams()
     write(d,"densidad = $p\n")
     write(d,"radio = $r0\n");
     write(d,"f = $f\n");
-    write(d,"ruido geometrico = $hg\n");
-    write(d,"ruido topologico = $ht\n");
+    write(d,"ruido geometrico = $hs\n");
+    write(d,"ruido topologico = $hl\n");
     write(d,"peso geometrico = $w\n");
     write(d,"regimen de velocidad = $l\n");
     write(d,"iteraciones = $T\n");
+    write(d,"frec muestreo = $step\n");
 
     close(d)
 end
@@ -50,6 +51,7 @@ function MakeDir()
     try
         run(`mkdir ../$path`)
         run(`mkdir ../$path/dists`)
+        # run(`mkdir ../$path/adjs`)
     catch y
         println(typeof(y))
     end
@@ -86,8 +88,8 @@ const dt   = 1.0
 const v0   = 1.0
 const w    = 0.15 # Peso relativo de vecindades
 
-const ht   = 0.25
-const hg   = 0.25
+const hl   = 0.25
+const hs   = 0.25
 const p    = 0.8
 const L    = 20.0 # Tamaño caja inicial
 const l    = 0.35
@@ -95,32 +97,19 @@ const l    = 0.35
 const T    = 1000 #iteraciones
 const step = 250 #se recupera informacion cada step 
 
-
-
 const N = convert(Int64, L*L * p) # Numero de particulas (entero)
 
 r0 = v0 * dt / l
 
-ruido = [ht hg] # [largo corto]
+ruido = [hl,hs] # [largo,corto]
 
 # println(ruido)
 
 #Crea estructura de folders
 
-# path = "data_f$(f)_w$w"
-# path = "JuliaFlocks/Julia/DATA/data_f$(f)"
 path = "../DATA/data_f$(f)"
 
-# run(`if [ ! -d ""../$path""  ]; then
-#   mkdir ../$path
-#   mkdir ../$path/dists
-#     fi`)
-
 MakeDir()
-# run(`mkdir ../$path`)
-# run(`mkdir ../$path/dists`)
-# run(`mkdir ../$path/adjs`)
-
 PrintParams()
 
 k = convert(Int64,floor(f*N)) # Conectividad en funcion de la fraccion de particulas
@@ -131,7 +120,6 @@ println("Particulas = $N")
 println("Radio = $r0")
 println("Conectividad = $f")
 
-
 pos = Array{Float64,1}[] #Vector de posiciones
 vel = Array{Float64,1}[] #Vector de velocidades
 
@@ -139,66 +127,37 @@ vel = Array{Float64,1}[] #Vector de velocidades
 
 Dist = zeros(N,N) #Matriz de distancias
 
-# SR = zeros(N,N) #Interacciones de corto alcanze
-# LR = zeros(N,N) #Interacciones de lanrgo alcanze -> NO CAMBIA en tiempo
-
 #Usando sparse
-# SR = spzeros(N,N) #Interacciones de corto alcanze
-LR = spzeros(N,N) #Interacciones de lanrgo alcanze -> NO CAMBIA en tiempo
+LR = spzeros(N,N) #Interacciones de largo alcanze 
+                  #No cambia en el tiempo
 
 StartVecs(L,vel,pos)
 StartVels(v0,vel)
 
-# println(typeof(k))
-# println(typeof(LR))
-# println(typeof(SR))
-
 SetLR(k,LR)
 
-# println("$vel\n")
+# SR = SetSR(r0,Dist)
+# @time GetAngs(vel,SR)
+# @time GetAngs(vel,LR)
 
-# for i = 1:N
-
-#     println("original")
-#     println(vel[i])
-
-#     otro = Array(2*vel[i])
-    
-#     println("otro")
-#     println(otro)
-
-#     vel[i] = otro
-
-#     # vel[i] = 2*vel[i]
-
-#     println("asignado")
-#     println(vel[i])
-# end
+# @time UpdateVel(vel,SR,LR)
+# println(issparse(SR))
 
 for i = 1:T
-
-    # println("posiciones:\n $pos")
-    # println("velocidades:\n $vel")
-
-    # @time SetMatrix(r0,SR,Dist)
-    # @time SR = SetMatrix(r0,Dist)
+ 
     SR = SetSR(r0,Dist)
-    @time UpdatePos(pos,dt)
+
+    UpdatePos(pos,dt)
     UpdateVel(vel,SR,LR)
-    # @time UpdateVel(vel,SetMatrix(r0,Dist),LR)
 
     # println(i)
     
     if  i == 1 || i%step == 0
-        # println(i)
         println("writing")
+        println(i)
         PrintTrays(pos)
         PrintDist(i,Dist)
     end
-
-    # println("Long Range:\n $LR")
-    # println("Short Range:\n $SR")
-    # println("Distancias:\n $Dist")
 
 end
 
