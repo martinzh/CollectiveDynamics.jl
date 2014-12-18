95501756bc92ce1c7a69725da78a5ae606817372
Martin Zumaya <sstereo.vision@gmail.com>
2014-12-18 13:16:09 -0600

    parametro

--- PyData/bash_script.py
+++ PyData/bash_script.py
@@ -11,7 +11,7 @@ s = 0.05
 script.write("#!/bin/bash\n")
 
 for i in range(1,N+1):
-  if i%4 == 0:
+  if i%8 == 0:
     script.write( "julia simul.jl " + repr(round(i*s,2)) + "\n")
   else:
     script.write( "nohup julia simul.jl " + repr(round(i*s,2)) + " &\n")
