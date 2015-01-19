2b7454b022e1b57b3040f77351d1921d6b21f138
Martin Zumaya <sstereo.vision@gmail.com>
2015-01-19 11:51:32 -0600

    Modifica script de sed

--- PyData/sed_script.py
+++ PyData/sed_script.py
@@ -1,7 +1,10 @@
+# Script para modificar las "," por "\t" en las trayectorias y velocidades
+
 
 import sys
 
 nombre = '../../DATA/sed_script.sh'
+# nombre = '../sed_script.sh'
 
 script = open(nombre,'w')
 
@@ -11,10 +14,8 @@ s = 0.05
 script.write("#!/bin/bash\n")
 
 for i in range(1,N+1):
-  if i%6 == 0:
-    script.write( "julia simul.jl " + repr(round(i*s,2)) + "\n")
-  else:
-    script.write( "nohup julia simul.jl " + repr(round(i*s,2)) + " &\n")
+  script.write( "sed 's/,/\t/g' " + "data_f" + repr(round(i*s,2)) + "/trays.txt > " + "data_f" + repr(round(i*s,2)) + "/trays_mod.txt\n")
+  script.write( "sed 's/,/\t/g' " + "data_f" + repr(round(i*s,2)) + "/vels.txt > "  + "data_f" + repr(round(i*s,2)) + "/vels_mod.txt\n")
 
 
 script.close()
