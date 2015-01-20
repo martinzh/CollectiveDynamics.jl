7677042b5fc2ccc1994486e1bf0918b0efa1ab9b
Martin Zumaya <sstereo.vision@gmail.com>
2015-01-20 12:21:12 -0600

    Agrerga

---
+++ PyData/bash_script.py
@@ -0,0 +1,27 @@
+
+import sys
+
+nombre = '../Julia/runs_script.sh'
+
+script = open(nombre,'w')
+
+start = float(sys.argv[1])
+stop  = float(sys.argv[2])
+step  = float(sys.argv[3])
+N     = int((stop - start)/step)
+
+
+print(N)
+
+script.write("#!/bin/bash\n")
+
+# for i in range(1,N+1):
+for i in range(N):
+  # if i%6 == 0:
+  if i%(N/2) == 0:
+    script.write( "julia simul.jl " + repr(start + round(i*step,4)) + "\n")
+  else:
+    script.write( "nohup julia simul.jl " + repr(start + round(i*step,4)) + " &\n")
+
+
+script.close()
