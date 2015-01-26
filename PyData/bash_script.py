183270fabe18c99e009cbffda873e9caf74fc485
martinzh <sstereo.vision@gmail.com>
2015-01-26 16:33:55 -0600

    Linea de comandos para script

--- PyData/bash_script.py
+++ PyData/bash_script.py
@@ -8,20 +8,30 @@ script = open(nombre,'w')
 start = float(sys.argv[1])
 stop  = float(sys.argv[2])
 step  = float(sys.argv[3])
-N     = int((stop - start)/step)
+T     = sys.argv[4]
+rate  = sys.argv[5]
+procs = float(sys.argv[6])
 
+N     = int((stop - start)/step)
 
 print(N)
 
 script.write("#!/bin/bash\n")
 
 # for i in range(1,N+1):
+# for i in range(N):
+#   # if i%6 == 0:
+#   if i%(N/2) == 0:
+#     script.write( "julia simul.jl " + repr(start + round(i*step,4)) + "\n")
+#   else:
+#     script.write( "nohup julia simul.jl " + repr(start + round(i*step,4)) + " &\n")
+
 for i in range(N):
   # if i%6 == 0:
-  if i%(N/2) == 0:
-    script.write( "julia simul.jl " + repr(start + round(i*step,4)) + "\n")
+  if i%(procs) == 0:
+    script.write( "julia simul_obj.jl " + repr(start + round(i*step,4)) + " " + T + " " + rate + "\n")
   else:
-    script.write( "nohup julia simul.jl " + repr(start + round(i*step,4)) + " &\n")
+    script.write( "nohup julia simul_obj.jl " + repr(start + round(i*step,4)) + " " + T + " " + rate + " &\n")
 
 
 script.close()
