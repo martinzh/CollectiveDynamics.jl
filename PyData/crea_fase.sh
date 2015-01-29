024339f5f7e0fdb9d51378c699b8c2129f09d403
martinzh <sstereo.vision@gmail.com>
2015-01-29 17:24:49 -0600

    Scripts para espacio fase

---
+++ PyData/crea_fase.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for i in {1..19}
+do
+
+  var=$(echo "0.05*$i" | bc -l)
+  # echo cls_data0"$var".dat
+  # echo awk '{print $2}' cls_data0$var.dat | tail -1 > tmp
+  awk '{print $2}' cls_data0$var.dat | tail -1 > tmp
+  echo "0$var" > tmp2
+  paste tmp2 tmp >> fase.dat
+done
+
+  # echo awk '{print $2}' cls_data1.0.dat | tail -1 > tmp
+  awk '{print $2}' cls_data1.0.dat | tail -1 > tmp
+  echo 1.0 > tmp2
+  paste tmp2 tmp >> fase.dat
+
+  rm tmp tmp2
---
+++ PyData/grafs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+python grafs_script.py 0.05
+gnuplot graf.gp
+python grafs_script.py 0.1
+gnuplot graf.gp
+python grafs_script.py 0.15
+gnuplot graf.gp
+python grafs_script.py 0.2
+gnuplot graf.gp
+python grafs_script.py 0.25
+gnuplot graf.gp
+python grafs_script.py 0.3
+gnuplot graf.gp
+python grafs_script.py 0.35
+gnuplot graf.gp
+python grafs_script.py 0.4
+gnuplot graf.gp
+python grafs_script.py 0.45
+gnuplot graf.gp
+python grafs_script.py 0.5
+gnuplot graf.gp
+python grafs_script.py 0.55
+gnuplot graf.gp
+python grafs_script.py 0.6
+gnuplot graf.gp
+python grafs_script.py 0.65
+gnuplot graf.gp
+python grafs_script.py 0.7
+gnuplot graf.gp
+python grafs_script.py 0.75
+gnuplot graf.gp
+python grafs_script.py 0.8
+gnuplot graf.gp
+python grafs_script.py 0.85
+gnuplot graf.gp
+python grafs_script.py 0.9
+gnuplot graf.gp
+python grafs_script.py 0.95
+gnuplot graf.gp
+python grafs_script.py 1.0
+gnuplot graf.gp
