c479a47c2d050656fec96798bc56b02e3f6891a4
Martin Zumaya <sstereo.vision@gmail.com>
2016-04-28 18:44:31 -0500

    test and debugging

--- new/pl.jl
+++ new/pl.jl
@@ -27,17 +27,19 @@ type Flock
     noise::SharedArray
     poski::SharedArray
     nij::SharedArray
+    rij_ids::SharedArray
 
     #Inicializa
     Flock(N::Int64, M::Int64) = new(
-        convert(SharedArray, zeros(2N)), # pos
-        convert(SharedArray, zeros(2N)), # vel
-        convert(SharedArray, zeros(2N)), # v_r
-        convert(SharedArray, zeros(2N)), # v_n
-        convert(SharedArray, zeros(N,N)),# rij
-        convert(SharedArray, zeros(N)),  # noise
-        convert(SharedArray, zeros(N + M)), # poski
-        convert(SharedArray, zeros(M)) # nij
+        convert( SharedArray, zeros(2N) ), # pos
+        convert( SharedArray, zeros(2N) ), # vel
+        convert( SharedArray, zeros(2N) ), # v_r
+        convert( SharedArray, zeros(2N) ), # v_n
+        convert( SharedArray, zeros(N, N) ),# rij
+        convert( SharedArray, zeros(N) ),  # noise
+        convert( SharedArray, zeros(N + M) ), # poski
+        convert( SharedArray, zeros(M) ), # nij
+        convert(SharedArray, Array{Int64}(div(N * (N-1), 2), 2)) #rij id's
     )
 
 end
@@ -70,10 +72,10 @@ end
         return 1:0, 1:0
     end
     nchunks = length(procs(q))
-    splits = [round(Int, s) for s in linspace(0,size(q,1),nchunks+1)]
+    splits = [round(Int, s) for s in linspace(0, size(q,1), nchunks+1)]
     # println(splits[idx]+1:splits[idx+1])
     # println(myid())
-    splits[idx]+1:splits[idx+1]
+    return splits[idx]+1:splits[idx+1]
 end
 
 ####===============================####
@@ -87,12 +89,33 @@ end
     nchunks = length(procs()[2:end])
     splits = [round(Int, s) for s in linspace(0, N, nchunks+1)]
     # println(splits[idx]+1:splits[idx+1], ", ", typeof(splits[idx]+1:splits[idx+1]) )
-    splits[idx]+1:splits[idx+1]
+    return splits[idx]+1:splits[idx+1]
 end
 
 ####===============================####
 
-@everywhere function rot_move_chunk(pos::SharedArray, vel::SharedArray, v_r::SharedArray, noise::SharedArray, dt::Float64, omega::Float64, eta::Float64, id_range::UnitRange)
+function calc_ranges(N::Int64)
+    nchunks = length(procs()[2:end])
+    splits = [round(Int, s) for s in linspace(0, N, nchunks+1)]
+    return UnitRange[splits[idx-1]+1:splits[idx] for idx in procs()[2:end]]
+end
+####===============================####
+
+function assign_ids(pair_dists::SharedArray, N::Int64)
+
+    k = 1
+
+    for i in 1:N, j in (i+1):N
+        pair_dists[k,1] = i
+        pair_dists[k,2] = j
+        k += 1
+    end
+
+end
+
+####===============================####
+
+@everywhere function rot_move_chunk(pos::SharedArray, vel::SharedArray, v_r::SharedArray, v_n::SharedArray, noise::SharedArray, dt::Float64, omega::Float64, eta::Float64, id_range::UnitRange)
 
     for i in id_range
 
@@ -107,20 +130,18 @@ end
             loc_angle = 0.0
         end
 
-        # tot_angle = omega * loc_angle + (1.0 - omega) * nonloc_angle + (noise[i] * 2.0 * pi - pi) * eta
+        # tot_angle = omega * loc_angle + (noise[i] * 2.0 * pi - pi) * eta
 
-        tot_angle = omega * loc_angle + (noise[i] * 2.0 * pi - pi) * eta
+        i_vx = v_n[2i - 1]
+        i_vy = v_n[2i]
 
-        # i_vx = nl_vel[2i - 1]
-        # i_vy = nl_vel[2i]
-        #
-        # if i_vx != 0.0 || i_vy != 0.0
-        #     nonloc_angle = atan2(i_vy, i_vx) - prop_angle
-        # else
-        #     nonloc_angle = 0.0
-        # end
+        if i_vx != 0.0 || i_vy != 0.0
+            nonloc_angle = atan2(i_vy, i_vx) - prop_angle
+        else
+            nonloc_angle = 0.0
+        end
 
-        # tot_angle = omega * loc_angle + (1.0 - omega) * nonloc_angle + (noise[i] * 2.0 * pi - pi) * eta
+        tot_angle = omega * loc_angle + (1.0 - omega) * nonloc_angle + (noise[i] * 2.0 * pi - pi) * eta
 
         vx = vel[2i - 1]*cos(tot_angle) - vel[2i]*sin(tot_angle)
         vy = vel[2i - 1]*sin(tot_angle) + vel[2i]*cos(tot_angle)
@@ -135,11 +156,11 @@ end
 
 end
 
-@everywhere rot_move(pos::SharedArray, vel::SharedArray, v_r::SharedArray, noise::SharedArray, dt::Float64, omega::Float64, eta::Float64, N::Int64) = rot_move_chunk(pos, vel, v_r, noise, dt, omega::Float64, eta::Float64, my_rij_range(N))
+@everywhere rot_move(pos::SharedArray, vel::SharedArray, v_r::SharedArray, v_n::SharedArray, noise::SharedArray, dt::Float64, omega::Float64, eta::Float64, N::Int64) = rot_move_chunk(pos, vel, v_r, v_n, noise, dt, omega, eta, my_rij_range(N))
 
 ####===============================####
 
-@everywhere function rot_move_chunk_bound(pos::SharedArray, vel::SharedArray, v_r::SharedArray, noise::SharedArray, dt::Float64, omega::Float64, eta::Float64, bound::Float64, id_range::UnitRange)
+@everywhere function rot_move_chunk_bound(pos::SharedArray, vel::SharedArray, v_r::SharedArray, v_n::SharedArray, noise::SharedArray, dt::Float64, omega::Float64, eta::Float64, bound::Float64, id_range::UnitRange)
 
     for i in id_range
 
@@ -154,18 +175,18 @@ end
             loc_angle = 0.0
         end
 
-        tot_angle = omega * loc_angle + (noise[i] * 2.0 * pi - pi) * eta
+        # tot_angle = omega * loc_angle + (noise[i] * 2.0 * pi - pi) * eta
 
-        # i_vx = nl_vel[2i - 1]
-        # i_vy = nl_vel[2i]
-        #
-        # if i_vx != 0.0 || i_vy != 0.0
-        #     nonloc_angle = atan2(i_vy, i_vx) - prop_angle
-        # else
-        #     nonloc_angle = 0.0
-        # end
+        i_vx = v_n[2i - 1]
+        i_vy = v_n[2i]
 
-        # tot_angle = omega * loc_angle + (1.0 - omega) * nonloc_angle + (noise[i] * 2.0 * pi - pi) * eta
+        if i_vx != 0.0 || i_vy != 0.0
+            nonloc_angle = atan2(i_vy, i_vx) - prop_angle
+        else
+            nonloc_angle = 0.0
+        end
+
+        tot_angle = omega * loc_angle + (1.0 - omega) * nonloc_angle + (noise[i] * 2.0 * pi - pi) * eta
 
         vx = vel[2i - 1]*cos(tot_angle) - vel[2i]*sin(tot_angle)
         vy = vel[2i - 1]*sin(tot_angle) + vel[2i]*cos(tot_angle)
@@ -193,22 +214,28 @@ end
 
 end
 
-@everywhere rot_move_bound(pos::SharedArray, vel::SharedArray, v_r::SharedArray, noise::SharedArray, dt::Float64, omega::Float64, eta::Float64, bound::Float64, N::Int64) = rot_move_chunk_bound(pos, vel, v_r, noise, dt, omega::Float64, eta::Float64, bound::Float64, my_rij_range(N))
+@everywhere rot_move_bound(pos::SharedArray, vel::SharedArray, v_r::SharedArray, v_n::SharedArray, noise::SharedArray, dt::Float64, omega::Float64, eta::Float64, bound::Float64, N::Int64) = rot_move_chunk_bound(pos, vel, v_r, v_n, noise, dt, omega, eta, bound, my_rij_range(N))
 
 ####===============================####
 
 @everywhere function calc_Rij_chunk(pos::SharedArray, rij::SharedArray, N::Int64, r0::Float64, id_range::UnitRange)
 
+    r02 = r0*r0
+
     for i in id_range, j in (i+1):N
 
         d =  (pos[2i - 1] - pos[2j - 1])^2 + (pos[2i] - pos[2j])^2
 
-        if d <= r0 * r0
-            # rij[j,i] = 1
+        if d <= r02
+            # rij[j,i] = 1.0
             rij[j,i] = rij[i,j] = 1.0
         else
+            # rij[j,i] = 0.0
             rij[j,i] = rij[i,j] = 0.0
         end
+
+        # rij[j, i] = rij[i, j] = 666.0
+
     end
 
 end
@@ -218,12 +245,45 @@ end
 
 ####===============================####
 
-# function dists(pos::SharedArray, rij::SharedArray, N::Int64, r0::Float64)
+@everywhere function calc_Rij_chunk_id(pos::SharedArray, rij::SharedArray, rij_ids::SharedArray, r0::Float64, id_range::UnitRange)
+
+    for k in id_range
+
+        i = rij_ids[k, 1]
+        j = rij_ids[k, 2]
+
+        d =  (pos[2i - 1] - pos[2j - 1])^2 + (pos[2i] - pos[2j])^2
+
+        if d <= r0 * r0
+            # rij[j,i] = 1.0
+            rij[j,i] = rij[i,j] = 1.0
+        else
+            # rij[j,i] = 0.0
+            rij[j,i] = rij[i,j] = 0.0
+        end
+
+        # rij[j,i] = 666.0
+    end
+
+end
+
+# WRAPPER
+@everywhere calc_Rij_id(pos::SharedArray, rij::SharedArray, rij_ids::Int64, r0::Float64, tot_dists::Int64) = calc_Rij_chunk(pos, rij, rij_ids, r0, my_rij_range(tot_dists))
+
+####===============================####
+
 function dists(flock::Flock, param::Param)
     @sync begin
         for p in procs()[2:end]
-            # @async remotecall_wait(p, calc_Rij, pos, rij, N, r0)
             @async remotecall_wait(p, calc_Rij, flock.pos, flock.rij, param.N, param.r0)
+        end
+    end
+end
+
+function dists_id(flock::Flock, param::Param, rij_ranges::Array{UnitRange})
+    @sync begin
+        for p in procs()[2:end]
+            @async remotecall_wait(p, calc_Rij_chunk_id, flock.pos, flock.rij, flock.rij_ids, param.r0, rij_ranges[p - 1])
         end
     end
 end
@@ -237,11 +297,24 @@ end
         vx = vel[2i - 1]
         vy = vel[2i]
 
-        ki = 1.0 + sum(rij.s[: ,i])
+        ki = 1.0 + sum(rij[: ,i])
 
-        for j in 1:N
-            vx += vel[2j - 1] * rij[j,i]
-            vy += vel[2j]     * rij[j,i]
+        # for j in 1:N
+        #
+        #     adj = rij[ j, i ]
+        #
+        #     vx += vel[2j - 1] * adj
+        #     vy += vel[2j]     * adj
+        #
+        #     # vx += vel[2j - 1] * rij[ j, i ]
+        #     # vy += vel[2j]     * rij[ j, i ]
+        # end
+
+        for j in findn(rij[:, i])
+
+            vx += vel[2j - 1]
+            vy += vel[2j]
+
         end
 
         v_r[2i - 1] = vx / ki
@@ -253,12 +326,51 @@ end
 @everywhere loc_vels(vel::SharedArray, v_r::SharedArray, rij::SharedArray, N::Int64) = loc_vels_chunk(vel, v_r, rij, N, my_rij_range(N))
 
 ####===============================####
+@everywhere function non_loc_vels_chunk(vel::SharedArray, v_n::SharedArray, nij::SharedArray, poski::SharedArray, id_range::UnitRange)
+
+    for i in id_range
+
+        vx = 0.0
+        vy = 0.0
+
+        ki = nij[ poski[i] ]
+
+        if ki > 0
+
+            for j in 1:ki
+
+                # k = nij[ poski[i] + j ]
+                #
+                # vx += vel[2k - 1]
+                # vy += vel[2k]
+
+                vx += vel[2 * nij[ poski[i] + j ] - 1]
+                vy += vel[2 * nij[ poski[i] + j ]]
+
+            end
+
+            v_n[2i - 1] = vx / convert(Float64, ki)
+            v_n[2i]     = vy / convert(Float64, ki)
+
+        else
+            v_n[2i - 1] = 0.0
+            v_n[2i]     = 0.0
+        end
+
+    end
+
+end
+
+@everywhere non_loc_vels(vel::SharedArray, v_n::SharedArray, nij::SharedArray, poski::SharedArray, N::Int64) = non_loc_vels_chunk(vel, v_n, nij, poski, my_rij_range(N))
+
+####===============================####
 
 function calc_vels(flock::Flock, param::Param)
 
     @sync begin
         for p in procs(pos)
             @async remotecall_wait(p, loc_vels, flock.vel, flock.v_r, flock.rij, param.N)
+            @async remotecall_wait(p, non_loc_vels, flock.vel, flock.v_n, flock.nij, flock.poski, param.N)
         end
     end
 
@@ -278,12 +390,16 @@ function evol(flock::Flock, param::Param, vals::Array{Float64,2}, T::Int64, dt::
         @sync begin
             for p in procs(flock.pos)
                 @async remotecall_wait(p, loc_vels, flock.vel, flock.v_r, flock.rij, param.N)
+                @async remotecall_wait(p, non_loc_vels, flock.vel, flock.v_n, flock.nij, flock.poski, param.N)
             end
         end
 
+        # flock.noise = convert(SharedArray, rand(param.N))
+        flock.noise = rand(param.N)
+
         @sync begin
             for p in procs(flock.pos)
-                @async remotecall_wait(p, rot_move, flock.pos, flock.vel, flock.v_r, flock.noise, dt, param.omega, param.eta, param.N)
+                @async remotecall_wait(p, rot_move, flock.pos, flock.vel, flock.v_r, flock.v_n, flock.noise, dt, param.omega, param.eta, param.N)
             end
         end
 
@@ -305,12 +421,76 @@ function evol_bound(flock::Flock, param::Param, vals::Array{Float64,2}, T::Int64
         @sync begin
             for p in procs(flock.pos)
                 @async remotecall_wait(p, loc_vels, flock.vel, flock.v_r, flock.rij, param.N)
+                @async remotecall_wait(p, non_loc_vels, flock.vel, flock.v_n, flock.nij, flock.poski, param.N)
+            end
+        end
+
+        flock.noise = rand(param.N)
+
+        @sync begin
+            for p in procs(flock.pos)
+                @async remotecall_wait(p, rot_move_bound, flock.pos, flock.vel, flock.v_r, flock.v_n, flock.noise, dt, param.omega, param.eta, param.bound, param.N)
+            end
+        end
+
+        vals[:,t] = flock.pos.s
+    end
+end
+
+####===============================####
+
+function evol_range(flock::Flock, param::Param, vals::Array{Float64,2}, T::Int64, dt::Float64, range::Array{UnitRange}, rij_range::Array{UnitRange})
+    for t in 1:T
+
+        @sync begin
+            for p in procs(flock.pos)
+                # @async remotecall_wait(p, calc_Rij_chunk, flock.pos, flock.rij, param.N, param.r0, range[p-1])
+                @async remotecall_wait(p, calc_Rij_chunk_id, flock.pos, flock.rij, flock.rij_ids, param.r0, rij_range[p-1])
             end
         end
 
         @sync begin
             for p in procs(flock.pos)
-                @async remotecall_wait(p, rot_move_bound, flock.pos, flock.vel, flock.v_r, flock.noise, dt, param.omega, param.eta, param.bound, param.N)
+                @async remotecall_wait(p, loc_vels_chunk, flock.vel, flock.v_r, flock.rij, param.N, range[p-1])
+                @async remotecall_wait(p, non_loc_vels_chunk, flock.vel, flock.v_n, flock.nij, flock.poski, range[p-1])
+            end
+        end
+
+        flock.noise = rand(param.N)
+
+        @sync begin
+            for p in procs(flock.pos)
+                @async remotecall_wait(p, rot_move_chunk, flock.pos, flock.vel, flock.v_r, flock.v_n, flock.noise, dt, param.omega, param.eta, range[p-1])
+            end
+        end
+
+        vals[:,t] = flock.pos.s
+    end
+end
+
+####===============================####
+
+function evol_bound_range(flock::Flock, param::Param, vals::Array{Float64,2}, T::Int64, dt::Float64, range::Array{UnitRange})
+    for t in 1:T
+
+        @sync begin
+            for p in procs(flock.pos)
+                @async remotecall_wait(p, calc_Rij_chunk, flock.pos, flock.rij, param.r0, range[p-1])
+            end
+        end
+
+        @sync begin
+            for p in procs(flock.pos)
+                @async remotecall_wait(p, loc_vels_chunk, flock.vel, flock.v_r, flock.rij, range[p-1])
+                @async remotecall_wait(p, non_loc_vels_chunk, flock.vel, flock.v_n, flock.nij, flock.poski, range[p-1])
+            end
+        end
+
+        flock.noise = rand(param.N)
+
+        @sync begin
+            for p in procs(flock.pos)
+                @async remotecall_wait(p, rot_move_bound_chunk, flock.pos, flock.vel, flock.v_r, flock.v_n, flock.noise, dt, param.omega, param.eta, param.bound, range[p-1])
             end
         end
 
@@ -338,4 +518,74 @@ function print_rij_range(N)
     end
 end
 
-####===============================####
+###========================================###
+### FUNCTIONS FOR CREATING INTERACTION NET ###
+###========================================###
+
+###========================================###
+
+function checkId(id::Int64, n::Int64)
+    for i in 0:( n-1 )
+        if in(id, ( i * n+1 ):( n * (i+1) ) )
+            return i+1
+        end
+    end
+end
+
+###========================================####
+
+function setLinks!(m::Int64, n::Int64, numLinks::Array{Int64, 1}, adj_vec::Array{Int64, 1})
+    for i in 1:m
+        while true
+            link = rand(1:n*n)
+            # link = int(rand(rngLinks)*n*n)+1
+            if adj_vec[link] < 1
+                adj_vec[link] = 1
+                numLinks[checkId(link, n)] += 1
+                break
+            end
+        end
+    end
+end
+
+###========================================####
+
+function reArrange(m::Int64, n::Int64, numLinks::Array{Int64, 1}, adj_vec::Array{Int64, 1})
+
+    Nij = zeros(Int64, m+n)
+    posNumLinks = zeros(Int64, n)
+
+    k = 1
+    for i in 1:n
+        Nij[k] = numLinks[i]
+        posNumLinks[i] = k
+        k += numLinks[i] + 1
+    end
+
+    links = find(x -> x==1, adj_vec)
+
+    k = 0
+    for i in 1:n
+        for j in 1:numLinks[i]
+            part = links[j+k] % n
+            if part != 0 # part - 1 -> [0,n-1] for GPU
+                Nij[posNumLinks[i]+j] = part
+            else
+                Nij[posNumLinks[i]+j] = n
+            end
+        end
+        k += numLinks[i]
+    end
+
+    return Nij, posNumLinks
+end
+
+###========================================####
+
+function make_IN(n::Int64, m::Int64)
+    adj_vec = reshape(2*eye(Int64, n), n*n)
+    numLinks = zeros(Int64, n)
+
+    setLinks!(m, n, numLinks, adj_vec)
+    return reArrange(m, n, numLinks, adj_vec)
+end
--- new/simul_test.jl
+++ new/simul_test.jl
@@ -42,12 +42,12 @@ elseif length(ARGS) == 0
 
     println("Usando valores por defecto");
 
-    param.N   = 3500
-    m_frac    = 0.1
+    param.N   = 1000
+    m_frac    = 0.0006
     steps     = 1000
     frec      = 50
-    param.eta = 0.05
-    rho       = 0.25
+    param.eta = 0.15
+    rho       = 10.0
     reps      = 5
 
 elseif length(ARGS) > 0 && length(ARGS) < 7
@@ -61,15 +61,15 @@ links     = m_frac * float(tot_links)
 param.M = round(Int, links)
 param.bound = sqrt( float(param.N) / rho)
 
-println(param)
-
 #////////////////////////////////////////////#
 #           INITIALIZE SYSTEM                #
 #////////////////////////////////////////////#
 
 flock = Flock(param.N, param.M)
 
-out_pos = zeros(Float64, 2 * param.N, 1000)
+T = 1000
+
+out_pos = zeros(Float64, 2 * param.N, T)
 
 # posiciones iniciales
 # particulas se inicializan en una caja de tamanio w
@@ -77,16 +77,38 @@ flock.pos = Float64[ -param.bound + rand() * 2.0 * param.bound for i in 1:(2 * p
 
 # velocidades
 # velocidad inicial con direccion aleatoria y norma v0
-init_vels(param.N, param.v0, flock.vel)
+@time init_vels(param.N, param.v0, flock.vel)
+
+#inicializa red de interaccion no local
+nij, poski  = make_IN(param.N, param.M)
+flock.nij   = convert(SharedArray, nij)
+flock.poski = convert(SharedArray, poski)
+
+ranges     = calc_ranges(param.N)
+rij_ranges = calc_ranges(size(flock.rij_ids, 1))
+
+assign_ids(flock.rij_ids, param.N)
 
 flock.vel.s
 flock.pos.s
+flock.v_r.s
+flock.v_n.s
+flock.nij.s
+flock.poski.s
+flock.rij_ids.s
+flock.rij.s
 
 param.omega = 1.0
 param.eta = 0.0
 
+@time dists(flock, param)
+@time dists_id(flock, param, rij_ranges)
+
 # @time evol_bound(flock, param, out_pos, 100, dt)
-@time evol(flock, param, out_pos, 1000, dt)
+@time evol(flock, param, out_pos, T, dt)
+@time evol_range(flock, param, out_pos, T, dt, ranges, rij_ranges)
+
+typeof(ranges[1])
 
 print_range(flock.pos)
 print_rij_range(param.N)
@@ -94,7 +116,7 @@ print_rij_range(param.N)
 out_pos
 
 for i in 1:2:param.N
-    plt[:plot]( reshape(out_pos[i,:], 1000), reshape(out_pos[i+1,:], 1000), "-" )
+    plt[:plot]( reshape(out_pos[i,:], T), reshape(out_pos[i+1,:], T), "-" )
 end
 
 plt[:clf]()
