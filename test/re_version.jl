d4251eef789b839e49e2d0bd8289f4d56351f429
Martin Zumaya <sstereo.vision@gmail.com>
2016-12-01 17:45:15 -0600

    testing

--- test/re_version.jl
+++ test/re_version.jl
@@ -32,51 +32,27 @@ end
 ### SPIN UPDATE
 ### ============== ### ============== ### ============== ###
 
-function calc_rij(pos, rij)
+function calc_rij(pos, rij, r0)
 
     # compute nij entries
     for i in 1:size(rij,1), j in (i+1):size(rij,1)
-        rij[j,i] = sqrt((pos[i][1] - pos[j][1])^2 + (pos[i][2] - pos[j][2])^2)
+        # d = sqrt((pos[i][1] - pos[j][1])^2 + (pos[i][2] - pos[j][2])^2)
+
+        sqrt((pos[i][1] - pos[j][1])^2 + (pos[i][2] - pos[j][2])^2) < r0 && sqrt((pos[i][1] - pos[j][1])^2 + (pos[i][2] - pos[j][2])^2) > 0.0 ? rij[j,i] = 1.0 : rij[i, j] = 0.0
         rij[i,j] = rij[j,i]
     end
 
 end
 
-function part_loc_vel(vel, rij, r0)
-
-    k = 0.0
-    vx = 0.0
-    vy = 0.0
-
-    for i in 1:size(rij,1)
-        if rij[i] <= r0 && rij[i] > 0.0
-            vx += vel[i][1]
-            vy += vel[i][2]
-            k += 1.0
-        else
-            vx += 0.0
-            vy += 0.0
-        end
-    end
-
-    print(k)
-
-    k > 0.0 ? [vx / k, vy / k] : [vx, vy]
-end
-
-function part_non_loc_vel(vel, Nij)
+function part_inter_vel(vel, Nij)
     sum(Nij) > 0.0 ? sum(map(*, vel, Nij)) / sum(Nij) : sum(map(*, vel, Nij))
 end
 
-function calc_interactions(vel, v_r, v_n, rij, Nij, r0)
+function calc_interactions(vel, v_r, v_n, rij, Nij)
 
     for i in 1:size(Nij, 1)
-        # v_r[i] = part_loc_vel(vel, view(rij, :, i))
-
-        v_r[i] = mean([vel[j] for j in find(x -> x > 0.0 && x < r0, view(rij, : ,i))])
-
-
-        v_n[i] = part_non_loc_vel(vel, view(Nij, :, i))
+        v_r[i] = part_inter_vel(vel, view(rij, :, i))
+        v_n[i] = part_inter_vel(vel, view(Nij, :, i))
     end
 
 end
@@ -126,12 +102,13 @@ end
 
 function evolve(pos, vel, v_r, v_n, rij, Nij, r0)
 
-    calc_rij(pos, rij)
-    calc_interactions(vel, v_r, v_n, rij,  Nij, r0)
+    calc_rij(pos, rij, r0)
+    calc_interactions(vel, v_r, v_n, rij,  Nij)
     map(rot_move_part, pos, vel, v_r, v_n)
 
 end
 
+# @time calc_rij(pos, rij, r0)
 # @time part_loc_vel(vel, view(rij, :, 1), pars.r0)
 # @time part_non_loc_vel(vel, view(Nij, :, 1))
 # @time test(vel, v_r, v_n, rij, Nij, pars.r0)
@@ -143,7 +120,7 @@ end
 ### =============== ### =============== ### =============== ###
 
 # N  = 128 # number of particles
-N  = 1024 # number of particles
+N  = 512 # number of particles
 κ = 7 # average non-local interactions
 η = 0.15 # noise intensity
 ρ  = 0.3 # density
@@ -169,8 +146,6 @@ p = κ / (N-1)
 # pos = [ [2*rand()*L - L, 2*rand()*L - L, 2*rand()*L - L] for i in 1:pars.N ]
 pos = [ [2*rand()*L - L, 2*rand()*L - L] for i in 1:N ]
 
-# pos = [ 2*rand()*L - L for i in 1:2*N ]
-
 # array of particles' velocities
 # vel = v0 * [ normalize([2*rand() - 1, 2*rand() - 1, 2*rand() - 1]) for i in 1:N ]
 vel = v0 * [ normalize([2*rand() - 1, 2*rand() - 1]) for i in 1:N ]
@@ -191,10 +166,47 @@ for i in 1:N, j in union(1:i-1, i+1:N)
 end
 
 ### ============== ### ============== ### ============== ###
+### OUTPUT
+### ============== ### ============== ### ============== ###
+
+pos_file = open("pos.bin", "r+")
+
+### ============== ### ============== ### ============== ###
 ### SYSTEM EVOLUTION
 ### ============== ### ============== ### ============== ###
 
-for t in 1:30
+times = [convert(Int, exp10(i)) for i in 0:4]
+
+for i in 1:(length(times)-1)
+
+    if i > 1
+
+        for t in (times[i]+1):times[i+1]
+
+            # evolve(pos, vel, v_r, v_n, rij, Nij, r0)
+
+            if t % times[i] == 0 || t % times[i-1] == 0
+                println("//////// ", t)
+            end
+        end
+
+    else
+
+        for t in (times[i]+1):times[i+1]
+
+            # evolve(pos, vel, v_r, v_n, rij, Nij, r0)
+
+            if t % times[i] == 0
+                println("//////// ", t)
+            end
+        end
+
+    end
+
+end
+
+
+for t in 1:100
     println(t, "\t", pos[1])
     evolve(pos, vel, v_r, v_n, rij, Nij, r0)
 end
