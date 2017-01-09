4c292f13392858e71ffadcbda4a9cad79643c366
Martin Zumaya <sstereo.vision@gmail.com>
2017-01-09 11:01:17 -0600

    data analysis

---
+++ new/trays_sim.jl
@@ -0,0 +1,205 @@
+### ============== ### ============== ### ============== ###
+## Numerical simulations of the Discretized version of the
+## inertial spin model Reference: Cavagna...
+## Martin Zumaya Hernandez
+## 14 / 11 / 2016
+### ============== ### ============== ### ============== ###
+
+### ============== ### ============== ### ============== ###
+###                 MODEL IMPLEMENTATION
+### ============== ### ============== ### ============== ###
+
+function calc_rij(pos, r0)
+
+    rij = zeros(length(pos), length(pos))
+
+    # compute rij entries
+    for i in 1:size(rij,1), j in (i+1):size(rij,1)
+
+        d = sqrt((pos[i][1] - pos[j][1])^2 + (pos[i][2] - pos[j][2])^2)
+
+        d < r0 && d > zero(Float64) ? rij[j,i] = one(Float64) : rij[j,i] = zero(Float64)
+
+        rij[i,j] = rij[j,i]
+    end
+
+    return rij
+end
+
+function set_Nij(p, Nij)
+
+    N = size(Nij, 1)
+
+    for i in 1:N, j in union(1:i-1, i+1:N)
+        rand() < p ? Nij[j, i] = one(Float64) : Nij[j, i] = zero(Float64)
+    end
+end
+
+function calc_interactions(vel, v_n, sp_Nij)
+
+    rows = rowvals(sp_Nij)
+
+    for i in 1:size(sp_Nij, 1)
+
+        range = nzrange(sp_Nij, i)
+
+        length(range) > 0 ? v_n[i] = mean([vel[rows[j]] for j in range ] ) : v_n[i] = zeros(2)
+
+    end
+
+end
+
+function rot_move_part(pos, vel, v_r, v_n)
+
+    prop_angle = atan2(vel[2], vel[1])
+
+    loc_angle = 0.0
+    non_loc_angle = 0.0
+
+    i_vx = v_r[1]
+    i_vy = v_r[2]
+
+    i_vx != 0.0 || i_vy != 0.0 ? loc_angle = atan2(i_vy, i_vx) - prop_angle : loc_angle = 0.0
+
+    i_vx = v_n[1]
+    i_vy = v_n[2]
+
+    i_vx != 0.0 || i_vy != 0.0 ? non_loc_angle = atan2(i_vy, i_vx) - prop_angle : non_loc_angle = 0.0
+
+    # total_angle = pars.ω * loc_angle + (1 - pars.ω) * non_loc_angle + pars.η * (2*rand()*pi - pi);
+
+    total_angle = 0.5 * (loc_angle + non_loc_angle) + 0.15 * (2 * rand() * pi - pi);
+
+    c = cos(total_angle)
+    s = sin(total_angle)
+
+    vx = vel[1]*c - vel[2]*s;
+    vy = vel[1]*s + vel[2]*c;
+
+    vel[1] = vx;
+    vel[2] = vy;
+
+    pos[1] += vx;
+    pos[2] += vy;
+
+end
+
+### ============== ### ============== ### ============== ###
+###                 SYSTEM EVOLUTION
+### ============== ### ============== ### ============== ###
+
+function evolve(pos, vel, v_r, v_n, sp_Nij, r0)
+
+    calc_interactions(vel, v_r, sparse(calc_rij(pos, r0)) )
+    calc_interactions(vel, v_n, sp_Nij)
+
+    map(rot_move_part, pos, vel, v_r, v_n)
+
+end
+
+### =============== ### =============== ### =============== ###
+### DEFINITION OF INITIAL PARAMETERS
+### =============== ### =============== ### =============== ###
+
+
+N = parse(Int, ARGS[1]) # number of particles
+# N  = 1024
+# N  = 512
+
+κ = parse(Int, ARGS[2]) # average non-local interactions
+# κ = 2
+
+T = parse(Int, ARGS[3]) # integration time steps
+
+rep = parse(Int, ARGS[4])
+
+# η = 0.15 # noise intensity
+
+ρ  = 0.3 # density
+L  = sqrt(N / ρ) # size of box
+
+l = 0.1 # Regimen de velocidad
+dt = 1.0 # Time integration step
+
+v0 = 1.0 # particle's speed
+
+r0 = (v0 * dt) / l
+
+p = κ / (N-1)
+
+# times = [convert(Int, exp10(i)) for i in 0:T]
+
+### ============== ### ============== ### ============== ###
+### OUTPUT
+### ============== ### ============== ### ============== ###
+
+folder_path = "../DATA/data_N_$(N)_k_$(κ)_trays"
+try
+    mkdir(folder_path)
+catch error
+    println("Folder already exists, not created")
+end
+
+# @time evolve(pos, vel, v_r, v_n, sp_Nij, r0)
+#
+# @time calc_rij(pos, rij, r0)
+# sp_rij = sparse(rij)
+#
+# @time calc_interactions(vel, v_r, sp_rij)
+# @time calc_interactions(vel, v_n, sp_Nij)
+#
+# @time pmap(rot_move_part, pos, vel, v_r, v_n; distributed=false)
+# @time pmap(rot_move_part, pos, vel, v_r, v_n)
+
+# for r in 1:reps
+
+println("rep: $(rep)")
+
+### =============== ### =============== ### =============== ###
+### INITIALIZATION OF PARTICLES INITIAL POSITIONS AND VELOCIDITES
+### =============== ### =============== ### =============== ###
+
+# array of random initial particles' postitions
+pos = [ [2*rand()*L - L, 2*rand()*L - L] for i in 1:N ]
+
+# array of particles' velocities
+vel = v0 * [ normalize([2*rand() - 1, 2*rand() - 1]) for i in 1:N ]
+
+# local metric interactions
+v_r = [zeros(2) for i in 1:N]
+
+# non local topological interactions
+v_n = [zeros(2) for i in 1:N]
+
+# non-local interaction network definition
+Nij = zeros(N, N)
+set_Nij(p, Nij)
+
+sp_Nij = sparse(Nij)
+
+# println("Ended Init")
+
+pos_file = open(folder_path * "/pos_$(rep).dat", "w+")
+vel_file = open(folder_path * "/vel_$(rep).dat", "w+")
+net_file = open(folder_path * "/net_$(rep).dat", "w+")
+
+write(net_file, Nij)
+close(net_file)
+
+### ============== ### ============== ### ============== ###
+### SYSTEM EVOLUTION
+### ============== ### ============== ### ============== ###
+
+for t in 1:Int(exp10(T))
+
+    write(pos_file, vcat(pos...))
+    write(vel_file, vcat(vel...))
+
+    evolve(pos, vel, v_r, v_n, sp_Nij, r0)
+
+end
+
+close(pos_file)
+close(vel_file)
+
+println("Done all")
--- test/data_analysis.jl
+++ test/data_analysis.jl
@@ -1,44 +1,116 @@
+ENV["PLOTS_USE_ATOM_PLOTPANE"] = "false"
+
 using StatPlots
 
 N = 1024
-κ = 5
+κ = 15
+T = 7
 
-folder_path = "DATA/data_N_$(N)_k_$(κ)"
+folder_path = "data_N_$(N)_k_$(κ)"
 
-r = 5
+means = Array{Float64}[]
 
-raw_data = reinterpret(Float64,read(folder_path * "/pos_$(r).dat"))
+# r = 5
 
-pos_data = reshape(raw_data, 2N, div(length(raw_data), 2N))
+for r in 1:15
 
-raw_data = reinterpret(Float64,read(folder_path * "/vel_$(r).dat"))
+    println(r)
 
-vel_data = reshape(raw_data, 2N, div(length(raw_data), 2N))
+    raw_data = reinterpret(Float64,read(folder_path * "/pos_$(r).dat"))
 
-raw_data = reinterpret(Float64,read(folder_path * "/net_$(r).dat"))
+    pos_data = reshape(raw_data, 2N, div(length(raw_data), 2N))
 
-int_net = reshape(raw_data, N, N)
+    push!(means, [mean(calc_rij_vect(pos_data[:, i])) for i in 1:size(pos_data,2)])
 
-anim = Animation()
+end
 
-for t in 1:size(pos_data, 2)
+# raw_data = reinterpret(Float64,read(folder_path * "/vel_$(r).dat"))
+#
+# vel_data = reshape(raw_data, 2N, div(length(raw_data), 2N))
+#
+# raw_data = reinterpret(Float64,read(folder_path * "/net_$(r).dat"))
+#
+# int_net = reshape(raw_data, N, N)
+#
+# anim = Animation()
+#
+# for t in 1:size(pos_data, 2)
+#
+#     println(t)
+#
+#     x = [pos_data[i, t] for i in 1:2:2N]
+#     y = [pos_data[i+1, t] for i in 1:2:2N]
+#
+#     pts = vec(P2[(x[i],y[i]) for i in 1:N])
+#
+#     # overlapping graphs
+#     # quiver!(pts, quiver = ([vel[i][1] for i in 1:N], [vel[i][2] for i in 1:N]))
+#
+#     # overwriting graphs
+#     quiver(pts, quiver = ([vel_data[i, t] for i in 1:2:2N], [vel_data[i+1, t] for i in 1:2:2N]))
+#
+#     frame(anim)
+#
+# end
+#
+#
+# gif(anim, "test.gif", fps = 24)
 
-    println(t)
+### ================================== ###
+### ================================== ###
 
-    x = [pos_data[i, t] for i in 1:2:2N]
-    y = [pos_data[i+1, t] for i in 1:2:2N]
+function calc_rij_vect(vect)
 
-    pts = vec(P2[(x[i],y[i]) for i in 1:N])
+    n = div(length(vect),2)
 
-    # overlapping graphs
-    # quiver!(pts, quiver = ([vel[i][1] for i in 1:N], [vel[i][2] for i in 1:N]))
+    # println(size(vect))
 
-    # overwriting graphs
-    quiver(pts, quiver = ([vel_data[i, t] for i in 1:2:2N], [vel_data[i+1, t] for i in 1:2:2N]))
+    vec_rij = zeros(Float64, div(n*(n-1),2) )
 
-    frame(anim)
+    k = 1
+
+    for i in 1:2:2n, j in (i+2):2:2n
+
+        vec_rij[k] = sqrt( (vect[i] - vect[j])^2 + (vect[i+1] - vect[j+1])^2)
+
+        k += 1
+    end
+
+    # println("Finish calc rij")
+
+    return vec_rij
+
+end
+
+times = [convert(Int, exp10(i)) for i in 0:T]
+
+mean_rij = [mean(calc_rij_vect(pos_data[:, i])) for i in 1:size(pos_data,2)]
+
+tau = Int64[]
+
+for i in 1:(length(times) - 1)
+
+    if i > 1
+
+        for t in (times[i]+1):times[i+1]
+
+            if t % times[i] == 0 || t % times[i-1] == 0
+                push!(tau, t)
+            end
+        end
+
+    else
+
+        for t in (times[i]+1):times[i+1]
+
+            if t % times[i] == 0
+                push!(tau, t)
+            end
+        end
+
+    end
 
 end
 
 
-gif(anim, "test.gif", fps = 24)
+plot(tau, mean(hcat(means...), 2), xscale = :log, yscale = :log)
