1cb179cdd61a1bbd079aad8aaa034ac1c30d6fb4
martinzh <sstereo.vision@gmail.com>
2017-03-01 10:45:25 -0600

    added time in script

--- turn_flock/scripts/reps.sh
+++ turn_flock/scripts/reps.sh
@@ -5,8 +5,8 @@ e="$2"
 n="$3"
 T="$4"
 
-for i in {1..5}
+for i in {1..10}
 do
   # nohup julia turn_flock_model.jl $N $k $T $i &
-  nohup julia ../test/test_model.jl $N $e $n $T $i &
+ time  nohup julia ../test/test_model.jl $N $e $n $T $i &
 done
