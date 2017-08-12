4b146c70c0009ddc447745cb440384ef985eaabc
martinzh <sstereo.vision@gmail.com>
2017-08-12 11:18:03 -0500

    added full path to julia

--- scripts/reps_scripts/all_k_top_2D.sh
+++ scripts/reps_scripts/all_k_top_2D.sh
@@ -3,19 +3,19 @@
 N="$1"
 T="$2"
 
-bash reps_locNloc_top_2D.sh $N 0.0 0.5 $T
+bash /home/martin/Code/julia/julia reps_locNloc_top_2D.sh $N 0.0 0.5 $T
 wait
-bash reps_locNloc_top_2D.sh $N 0.01 0.5 $T
+bash /home/martin/Code/julia/julia reps_locNloc_top_2D.sh $N 0.01 0.5 $T
 wait
-bash reps_locNloc_top_2D.sh $N 0.025 0.5 $T
+bash /home/martin/Code/julia/julia reps_locNloc_top_2D.sh $N 0.025 0.5 $T
 wait
-bash reps_locNloc_top_2D.sh $N 0.05 0.5 $T
+bash /home/martin/Code/julia/julia reps_locNloc_top_2D.sh $N 0.05 0.5 $T
 wait
-bash reps_locNloc_top_2D.sh $N 0.1 0.5 $T
+bash /home/martin/Code/julia/julia reps_locNloc_top_2D.sh $N 0.1 0.5 $T
 wait
-bash reps_locNloc_top_2D.sh $N 0.25 0.5 $T
+bash /home/martin/Code/julia/julia reps_locNloc_top_2D.sh $N 0.25 0.5 $T
 wait
-bash reps_locNloc_top_2D.sh $N 0.5 0.5 $T
+bash /home/martin/Code/julia/julia reps_locNloc_top_2D.sh $N 0.5 0.5 $T
 wait
-bash reps_locNloc_top_2D.sh $N 1.0 0.5 $T
+bash /home/martin/Code/julia/julia reps_locNloc_top_2D.sh $N 1.0 0.5 $T
 wait
