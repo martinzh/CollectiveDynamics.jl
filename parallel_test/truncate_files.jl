dc55127d3825e227cf87db8f164ef0c2b6fc0a32
Martin Zumaya <sstereo.vision@gmail.com>
2017-11-16 12:19:49 -0600

    tests

--- parallel_test/truncate_files.jl
+++ parallel_test/truncate_files.jl
@@ -1,11 +1,12 @@
 
 
-
 f = ARGS[1]
 N = ARGS[2]
 
 # f = "NLOC_P_MET_3D"
 # N = 512
+
+tr_ln = 3N * 369
 
 data_path = joinpath(homedir(),"art_DATA",f,"DATA", "data_N_$(N)")
 
@@ -13,7 +14,7 @@ for (root, dirs, files) in walkdir(data_path)
     println("Files in $root")
     for file in files
         # println(joinpath(root, file)) # path to files 
-        l = length(reinterpret(Float64, read(joinpath(root, file))))
-        println(l)
+        ln = length(reinterpret(Float64, read(joinpath(root, file))))
+        println(ln, "\t", l < tr_ln)
     end
 end
