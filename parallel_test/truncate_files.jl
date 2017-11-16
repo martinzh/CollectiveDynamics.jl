821b80ef15a42b13778df1ac6ceb983c3e268d8a
Martin Zumaya <sstereo.vision@gmail.com>
2017-11-16 12:31:52 -0600

    test

--- parallel_test/truncate_files.jl
+++ parallel_test/truncate_files.jl
@@ -1,4 +1,5 @@
 
+include("par_mod.jl")
 
 f = ARGS[1]
 N = parse(Int, ARGS[2])
@@ -10,11 +11,21 @@ tr_ln = 3N * 369
 
 data_path = joinpath(homedir(),"art_DATA",f,"DATA", "data_N_$(N)")
 
+output_path = set_output_data_structure_lnl(f*"_TRUNC", N, κ, ω)
+
 for (root, dirs, files) in walkdir(data_path)
     println("Files in $root")
     for file in files
         # println(joinpath(root, file)) # path to files 
-        ln = length(reinterpret(Float64, read(joinpath(root, file))))
-        println(file, "\t", ln, "\t", ln < tr_ln)
+        data = reinterpret(Float64, read(joinpath(root, file)))
+        ln = length(data)
+        # println(file, "\t", ln, "\t", ln < tr_ln)
+
+        if ln < tr_ln == false
+            println(file)
+            write(joinpath(output_path,file), data[1:tr_ln])
+        end
     end
 end
+
+println("Done")
