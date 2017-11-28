a4e2178c80c8c434479dba8e04b74ab10225ecc2
Martin Zumaya <sstereo.vision@gmail.com>
2017-11-28 16:49:52 -0600

    tests

--- parallel_test/expansion_folder_par.jl
+++ parallel_test/expansion_folder_par.jl
@@ -71,7 +71,7 @@ function calc_Rij_3D(pos::SharedArray, Rij::SharedArray)
 
     N = size(Rij, 1)
 
-    @parallel for i in 1:3:3N
+    for i in 1:3:3N
 
         for j in (i+3):3:3N
 
@@ -86,24 +86,6 @@ end
 
 ### ================================== ###
 
-@everywhere function calc_means(pos, vel, mean, nn_mean, vel_mean, N)
-
-    Rij = zeros(N, N)
-
-    for j in localindexes(mean)[1]
-
-        @show j
-
-        calc_Rij_3D(pos[:L][:,j], Rij)
-
-        mean[:L][j] = mean(Symmetric(Rij, :L))
-        nn_mean[:L][j] = mean(sort(Symmetric(Rij, :L), 1)[2,:])
-        vel_mean[:L][j] = norm(mean([[vel_data[:L][i, j], vel_data[:L][i+1, j], vel_data[:L][i+2, j]] for i in 1:3:3N]))
-
-    end
-end
-### ================================== ###
-
 @everywhere function myrange(q::SharedArray)
     idx = indexpids(q)
     if idx == 0 # This worker is not assigned a piece
@@ -112,6 +94,25 @@ end
     nchunks = length(procs(q))
     splits = [round(Int, s) for s in linspace(0,size(q,2),nchunks+1)]
     1:size(q,1), splits[idx]+1:splits[idx+1]
+end
+
+### ================================== ###
+
+@everywhere function calc_means(pos, vel, mean, nn_mean, vel_mean, N)
+
+    Rij = zeros(N, N)
+
+    j_range = myrange(pos)[2]
+
+    for j in j_range
+        # @show j
+        calc_Rij_3D(pos[:,j], Rij)
+
+        mean[j] = mean(Symmetric(Rij, :L))
+        nn_mean[j] = mean(sort(Symmetric(Rij, :L), 1)[2,:])
+        vel_mean[j] = norm(mean([[vel_data[i, j], vel_data[i+1, j], vel_data[i+2, j]] for i in 1:3:3N]))
+
+    end
 end
 
 ### ================================== ###
@@ -128,11 +129,11 @@ k = ARGS[3]
 # folder = "SVM_GRID_3D"
 # folder = "SVM_GRID_FN_2D"
 
-folder = "NLOC_MET_3D_EXT"
+# folder = "NLOC_MET_3D_EXT"
 # folder = "new/NLOC_MET_3D_EXT"
 # folder = "new/NLOC_TOP_3D_EXT"
 
-N = 4096
+# N = 4096
 # N = 4000
 # N = 1024
 # N = 512
@@ -143,7 +144,7 @@ N = 4096
 # k = "9.0"
 # k = "0.5"
 # k = "0.001"
-k = "0.0125"
+# k = "0.0125"
 # w = "0.5"
 
 w = "0.5"
@@ -174,18 +175,32 @@ nn_mean_file    = open(output_folder_path * "/exp_data_N_$(N)" * "/nn_mean" * pa
 
 ### ================================== ###
 
+sh_mean     = SharedArray{Float64}(length(times)-1)
+sh_nn_mean  = SharedArray{Float64}(length(times)-1)
+sh_vel_mean = SharedArray{Float64}(length(times)-1)
+
 reps = [match(r"\w+_(\d+).\w+", x).captures[1] for x in filter(x -> ismatch(r"pos_\d+.\w+", x), readdir(data_folder_path))]
 
-r = 2
+# r = 2
 for r in reps
 
     println("rep=",r)
 
     raw_data = reinterpret(Float64,read(data_folder_path * "/pos_$(r).dat"))
-    pos_data = reshape(raw_data, 3N, div(length(raw_data), 3N))
+
+    if length(raw_data) == 3N*(length(times)-1)
+        pos_data = reshape(raw_data, 3N, div(length(raw_data), 3N))
+    else
+        pos_data = reshape(raw_data[3N+1:end], 3N, div(length(raw_data[3N+1:end]), 3N))
+    end
 
     raw_data = reinterpret(Float64,read(data_folder_path * "/vel_$(r).dat"))
-    vel_data = reshape(raw_data, 3N, div(length(raw_data), 3N))
+
+    if length(raw_data) == 3N*(length(times)-1)
+        vel_data = reshape(raw_data, 3N, div(length(raw_data), 3N))
+    else
+        vel_data = reshape(raw_data[3N+1:end], 3N, div(length(raw_data[3N+1:end]), 3N))
+    end
 
     sh_pos = SharedArray{Float64,2}(size(pos_data))
     sh_vel = SharedArray{Float64,2}(size(vel_data))
@@ -197,29 +212,19 @@ for r in reps
 
     # d_pos = distribute(reshape(raw_data, 3N, div(length(raw_data), 3N)), procs = workers(), dist = [1,length(workers())])
 
-    sh_mean     = SharedArray{Float64}(length(times))
-    sh_nn_mean  = SharedArray{Float64}(length(times))
-    sh_vel_mean = SharedArray{Float64}(length(times))
-
-    @sync for p in workers()
-        @async remotecall_wait(calc_means, p, d_pos, d_vel, d_mean, d_nn_mean, d_vel_mean, N)
+    @sync begin
+        for p in workers()
+            @async remotecall_wait(calc_means, p, d_pos, d_vel, d_mean, d_nn_mean, d_vel_mean, N)
+        end
     end
-
-    # calc_means(d_pos, d_vel, d_mean, d_nn_mean, d_vel_mean, N)
 
     println("finished calc_means")
 
-    write(exp_file, convert(Array, d_mean))
-    write(nn_mean_file, convert(Array, d_nn_mean))
-    write(order_file, convert(Array, d_vel_mean))
+    write(exp_file, fetch(sh_mean))
+    write(nn_mean_file, fetch(sh_nn_mean))
+    write(order_file, fetch(sh_vel_mean))
 
 end
-
-close(d_pos)
-close(d_vel)
-close(d_mean)
-close(d_nn_mean)
-close(d_vel_mean)
 
 close(exp_file)
 close(nn_mean_file)
@@ -237,87 +242,82 @@ println("Done")
 #
 # plot(union(times[1:369],collect(2exp10(5):exp10(5):exp10(6))), [norm(mean([[vel_data[i, j], vel_data[i+1, j], vel_data[i+2, j]] for i in 1:3:3N])) for j in 1:size(vel_data, 2)], xscale = :log10, leg = false)
 
-pos_data
-sh_pos
-sh_vel
-
-
-@everywhere using DistributedArrays
-
-addprocs(4)
-
-N = 16
-
-t = rand(5, N)
-
-d_t = distribute(t, procs = workers(), dist = [1, length(workers())])
-
-d_s = dzeros(size(d_t,2))
-
-for p in workers()
-    # println(remotecall_fetch(myrange, p, sh_pos))
-    # println(remotecall_fetch(calc_means, p, sh_pos, sh_vel, sh_mean, sh_nn_mean, sh_vel, N))
-    println(p)
-    println(remotecall_fetch(myrange, p, sh_pos))
-    println(remotecall_fetch(myrange, p, sh_vel))
-    println(remotecall_fetch(localindexes, p, sh_mean))
-    println(remotecall_fetch(localindexes, p, sh_nn_mean))
-    println(remotecall_fetch(localindexes, p, sh_vel_mean))
-    println()
-end
-
-@everywhere function d_test(d)
-    println(localindexes(d))
-    println(d[:l])
-end
-
-@everywhere function d_test1(d)
-    for i in localindexes(d)[1]
-        d[i] = 10.
-    end
-end
-
-remotecall_fetch(d_test, 2, d_t)
-remotecall_fetch(d_test1, 2, d_s)
-
-DArray(size(d_s), procs(d_s)) do I
-    println(I)
-end
-
-size(sh_pos)
-
-@everywhere function myrange(q::SharedArray)
-    idx = indexpids(q)
-    if idx == 0 # This worker is not assigned a piece
-        return 1:0, 1:0
-    end
-    nchunks = length(procs(q))
-    splits = [round(Int, s) for s in linspace(0,size(q,2),nchunks+1)]
-    1:size(q,1), splits[idx]+1:splits[idx+1]
-end
-
-
-@everywhere function calc_means(pos, vel, mean, nn_mean, vel_mean, N)
-
-    # Rij = zeros(N, N)
-
-    j_range = myrange(pos)[2]
-
-    for j in j_range
-
-        # @show j
-        #
-        # calc_Rij_3D(pos[:,j], Rij)
-
-        # mean[j] = mean(Symmetric(Rij, :L))
-        # nn_mean[j] = mean(sort(Symmetric(Rij, :L), 1)[2,:])
-        # vel_mean[j] = norm(mean([[vel_data[i, j], vel_data[i+1, j], vel_data[i+2, j]] for i in 1:3:3N]))
-
-        mean[j] = myid()
-        # nn_mean[j] = mean(sort(Symmetric(Rij, :L), 1)[2,:])
-        # vel_mean[j] = norm(mean([[vel_data[i, j], vel_data[i+1, j], vel_data[i+2, j]] for i in 1:3:3N]))
-
-    end
-end
-
-sh_mean
+# pos_data
+# sh_pos
+# sh_vel
+#
+# @everywhere using DistributedArrays
+#
+# addprocs(4)
+#
+# N = 16
+#
+# t = rand(5, N)
+#
+# d_t = distribute(t, procs = workers(), dist = [1, length(workers())])
+#
+# d_s = dzeros(size(d_t,2))
+#
+# for p in workers()
+#     # println(remotecall_fetch(myrange, p, sh_pos))
+#     # println(remotecall_fetch(calc_means, p, sh_pos, sh_vel, sh_mean, sh_nn_mean, sh_vel, N))
+#     println(p)
+#     println(remotecall_fetch(myrange, p, sh_pos))
+#     println(remotecall_fetch(myrange, p, sh_vel))
+#     println(remotecall_fetch(localindexes, p, sh_mean))
+#     println(remotecall_fetch(localindexes, p, sh_nn_mean))
+#     println(remotecall_fetch(localindexes, p, sh_vel_mean))
+#     println()
+# end
+#
+# @everywhere function d_test(d)
+#     println(localindexes(d))
+#     println(d[:l])
+# end
+#
+# @everywhere function d_test1(d)
+#     for i in localindexes(d)[1]
+#         d[i] = 10.
+#     end
+# end
+#
+# remotecall_fetch(d_test, 2, d_t)
+# remotecall_fetch(d_test1, 2, d_s)
+#
+# DArray(size(d_s), procs(d_s)) do I
+#     println(I)
+# end
+#
+# size(sh_pos)
+#
+# @everywhere function myrange(q::SharedArray)
+#     idx = indexpids(q)
+#     if idx == 0 # This worker is not assigned a piece
+#         return 1:0, 1:0
+#     end
+#     nchunks = length(procs(q))
+#     splits = [round(Int, s) for s in linspace(0,size(q,2),nchunks+1)]
+#     1:size(q,1), splits[idx]+1:splits[idx+1]
+# end
+#
+#
+# @everywhere function calc_means(pos, vel, mean, nn_mean, vel_mean, N)
+#
+#     Rij = zeros(N, N)
+#
+#     j_range = myrange(pos)[2]
+#
+#     for j in j_range
+#         # @show j
+#         calc_Rij_3D(pos[:,j], Rij)
+#
+#         mean[j] = mean(Symmetric(Rij, :L))
+#         nn_mean[j] = mean(sort(Symmetric(Rij, :L), 1)[2,:])
+#         vel_mean[j] = norm(mean([[vel_data[i, j], vel_data[i+1, j], vel_data[i+2, j]] for i in 1:3:3N]))
+#
+#     end
+# end
+#
+# sh_mean
+#
+# println(remotecall_fetch(calc_means, 7, sh_pos, sh_vel, sh_mean, sh_nn_mean, sh_vel, N))
